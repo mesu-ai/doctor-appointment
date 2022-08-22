372e7d6f8331b0912a595d5ecdf6aa58eb998aef
Md Ibrahim Adham Mesu <ibrahim.mesu@gmail.com>
2022-08-23 00:10:51 +0600

    appoinment on progress"

--- src/pages/appointment/Appointment.jsx
+++ src/pages/appointment/Appointment.jsx
@@ -6,20 +6,29 @@ import moment from 'moment';
 
 const Appointment = () => {
 	// const [startDate, setStartDate] = useState(new Date());
-  const [startDate, setStartDate] = useState(null);
+	const [startDate, setStartDate] = useState(null);
 	const { doctor } = useLocation()?.state;
 
 	// function disabledDate(current) {
 	// 	// Can not select sundays and predfined days
 	// 	return moment(current).day() === 0;
 	// }
-  const date=new Date();
-  const isWeekday = (date) => {
-    const day = date.getDay(date);
-    return   day !== 1 &&  day !== 2 && day !==4 &&  day !==5 && day !==6 ;
-  };
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var dx = new Date('Sunday');
+var dayName = days[dx.getDay()];
+console.log(dayName);
+  
 
-	console.log(doctor);
+ const d=new Date('Friday');
+ console.log(d.getDay(d))
+	const date = new Date();
+	const isWeekday = (date) => {
+		const day = date.getDay(date);
+    // console.log(day);
+		return day !== 1 && day !== 2 && day !== 4 && day !== 5 && day !== 6;
+	};
+
+	// console.log(doctor);
 	return (
 		<div className='h-screen'>
 			<p>Appointment page</p>
@@ -28,24 +37,25 @@ const Appointment = () => {
 					<p>mesu</p>
 				</div>
 				<div className='  '>
-        <DatePicker
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      filterDate={isWeekday}
-      placeholderText="Select a weekday"
-    />
 					{/* <DatePicker
-            open
-            // todayButton="Today"
-            filterDate={isWeekday}
 						selected={startDate}
 						onChange={(date) => setStartDate(date)}
-						showTimeSelect
+						filterDate={isWeekday}
+						placeholderText='Select a weekday'
+					/> */}
+					<DatePicker
+						open
+						// todayButton="Today"
+						filterDate={isWeekday}
+						selected={startDate}
+						onChange={(date) => {setStartDate(date);
+                }}
+						// showTimeSelect
 						timeFormat='HH:mm'
 						timeIntervals={15}
 						timeCaption='time'
 						dateFormat='MMMM d, yyyy h:mm aa'
-					/> */}
+					/>
 				</div>
 			</div>
 		</div>
