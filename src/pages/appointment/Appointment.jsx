76790d7d39a16291e1f7ec658ab6ee9eb308367b
Md ibrahim adham mesu <mesu.s.ss@gmail.com>
2022-08-23 14:52:56 +0600

    upate disable date

--- src/pages/appointment/Appointment.jsx
+++ src/pages/appointment/Appointment.jsx
@@ -1,65 +1,116 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import moment from 'moment';
 
 const Appointment = () => {
-	// const [startDate, setStartDate] = useState(new Date());
-	const [startDate, setStartDate] = useState(null);
-	const { doctor } = useLocation()?.state;
+	const [available,setAvailable]=useState([]);
+  // const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(null);
+  const { doctor } = useLocation()?.state;
 
-	// function disabledDate(current) {
-	// 	// Can not select sundays and predfined days
-	// 	return moment(current).day() === 0;
-	// }
-  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-var dx = new Date('Sunday');
-var dayName = days[dx.getDay()];
-console.log(dayName);
-  
+	
 
- const d=new Date('Friday');
- console.log(d.getDay(d))
-	const date = new Date();
-	const isWeekday = (date) => {
-		const day = date.getDay(date);
-    // console.log(day);
-		return day !== 1 && day !== 2 && day !== 4 && day !== 5 && day !== 6;
-	};
+  // function disabledDate(current) {
+  // 	// Can not select sundays and predfined days
+  // 	return moment(current).day() === 0;
+  // }
+  console.log(doctor.availibility);
 
-	// console.log(doctor);
-	return (
-		<div className='h-screen'>
-			<p>Appointment page</p>
-			<div className='grid grid-cols-2'>
-				<div>
-					<p>mesu</p>
-				</div>
-				<div className='  '>
-					{/* <DatePicker
+  const weekdays = {
+    sun: 0,
+    mon: 1,
+    tue: 2,
+    wed: 3,
+    thu: 4,
+    fri: 5,
+    sat: 6,
+  };
+
+  const checkAvailableDay = () => {
+		const availableDays=[];
+   
+    for (const key in weekdays) {
+			 	const dayNumber = weekdays[key];
+		
+      for (const available in doctor.availibility) {
+        
+        if (available == key) {
+          // setAvailable(...available,dayNumber);
+					availableDays.push(dayNumber)
+          console.log('match');
+					
+        } else if (available != key) {
+          console.log('unmatch');
+        }
+      }
+    }
+		return availableDays;
+
+  };
+	
+
+	// console.log(availableDays);
+
+
+
+  // var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  // console.log(dayName);
+
+  // const d = new Date('Friday');
+  // console.log(d.getDay(d));
+  // const date = new Date();
+
+
+  const isWeekday = (date) => {
+		const result= checkAvailableDay();
+    const day = date.getDay(date);
+		// if(result.length >0){
+		// 	for (const iterator of result) {
+		// 		console.log(iterator);
+		// 		return day===iterator;
+				
+		// 	}
+		// }
+		return day===2;
+    
+  };
+
+  // console.log(doctor);
+  return (
+    <div className='h-screen'>
+      <p>Appointment page</p>
+      <div className='grid grid-cols-2'>
+        <div>
+          <p>mesu</p>
+        </div>
+        <div className='  '>
+          {/* <DatePicker
 						selected={startDate}
 						onChange={(date) => setStartDate(date)}
 						filterDate={isWeekday}
 						placeholderText='Select a weekday'
 					/> */}
-					<DatePicker
-						open
-						// todayButton="Today"
-						filterDate={isWeekday}
-						selected={startDate}
-						onChange={(date) => {setStartDate(date);
-                }}
-						// showTimeSelect
-						timeFormat='HH:mm'
-						timeIntervals={15}
-						timeCaption='time'
-						dateFormat='MMMM d, yyyy h:mm aa'
-					/>
-				</div>
-			</div>
-		</div>
-	);
+          <DatePicker
+            open
+            // todayButton="Today"
+            filterDate={isWeekday}
+            selected={startDate}
+            onChange={(date) => {
+              setStartDate(date);
+            }}
+            // showTimeSelect
+            timeFormat='HH:mm'
+            timeIntervals={15}
+            timeCaption='time'
+            dateFormat='MMMM d, yyyy h:mm aa'
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Appointment;
